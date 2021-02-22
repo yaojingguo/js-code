cfce352305af22cadb6b3e6e53291243cc19b63b
Jingguo Yao <yaojingguo@gmail.com>
2021-02-22 20:13:13 +0800

    update

--- react/js-yarn-demo/src/index.js
+++ react/js-yarn-demo/src/index.js
@@ -11,6 +11,16 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+import ClientMonitor from 'skywalking-client-js';
+// Report collected data to `http:// + window.location.host + /browser/perfData` in default
+ClientMonitor.register({
+  collector: 'http://127.0.0.1:8080',
+  service: 'test-ui',
+  pagePath: '/topics',
+  serviceVersion: 'v1.0.0',
+});
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
